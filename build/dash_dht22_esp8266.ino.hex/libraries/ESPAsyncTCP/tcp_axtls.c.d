5537b3db1d43b5aabd666312487f64c14a2892b4
michael <michael@cadot.eu>
2023-11-03 11:40:35 +0100

    maj with vscode

--- build/dash_dht22_esp8266.ino.hex/libraries/ESPAsyncTCP/tcp_axtls.c.d
+++
@@ -1,6 +0,0 @@
-/home/michael/git/arduino/dash_dht22_esp8266/build/dash_dht22_esp8266.ino.hex/libraries/ESPAsyncTCP/tcp_axtls.c.o: \
- /home/michael/Arduino/libraries/ESPAsyncTCP/src/tcp_axtls.c \
- /home/michael/.arduino15/packages/esp8266/hardware/esp8266/3.1.2/cores/esp8266/CommonHFile.h \
- /home/michael/git/arduino/dash_dht22_esp8266/build/dash_dht22_esp8266.ino.hex/core/dash_dht22_esp8266.ino.globals.h \
- /home/michael/Arduino/libraries/ESPAsyncTCP/src/async_config.h \
- /home/michael/Arduino/libraries/ESPAsyncTCP/src/DebugPrintMacros.h
